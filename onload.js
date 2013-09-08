764ff478892f23fe2d825b5997157625aaf04daa
miles <miles.okeefe@gmail.com>
2013-09-08 17:54:37 -0500

    mark letters as long or short

--- onload.js
+++ onload.js
@@ -11,12 +11,38 @@ function scan(input){
 	var result = "";
 	for(var i = 0; i < words.length; i++){
 		var word = words[i];
-		if(word.match('e')){
-			word = word.replace('e', '&#275;');
-		}
+
 		//var letters = word.match(/a|o|e|i|u/g);
 
 		result += word + " ";
 	}
 	return result;
+}
+function markLong(letter){
+	if(letter == 'a'){
+		return letter.replace('a', '&#257;');
+	}else if(letter == 'e'){
+		return letter.replace('e', '&#275;');
+	}else if(letter == 'i'){
+		return letter.replace('i', '&#299;');
+	}else if(letter == 'o'){
+		return letter.replace('o', '&#333;');
+	}else if(letter == 'u'){
+		return letter.replace('u', '&#363;');
+	}else if(letter == 'y'){
+		return letter.replace('y', '&#563;');
+	}
+}
+function markShort(letter){
+	if(letter == 'a'){
+		return letter.replace('a', '&#259;');
+	}else if(letter == 'e'){
+		return letter.replace('e', '&#277;');
+	}else if(letter == 'i'){
+		return letter.replace('i', '&#301;');
+	}else if(letter == 'o'){
+		return letter.replace('o', '&#335;');
+	}else if(letter == 'u'){
+		return letter.replace('u', '&#365;');
+	}
 }
